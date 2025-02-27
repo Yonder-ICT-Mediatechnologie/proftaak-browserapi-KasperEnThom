6766c3322557cb28f048c2677e0d3778c1786a97
Kasper Heuer <40208910@roctilburg.nl>
2025-02-27 09:55:16 +0100

    header begonnen

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Invoeren</title>
+</head>
+<style>
+.heading{
+    font-family: poest;
+}
+</style>
+<body>
+    <header>
+        <img src="logo.png" alt="Onze logo">
+        <div class="bedrijfNaam heading">MorseXpress</div>
+    </header>
+</body>
+
+</html>
