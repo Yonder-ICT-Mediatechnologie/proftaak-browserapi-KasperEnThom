d707c721a1a21fefeede44ff3a90453d7e95e6a4
Kasper Heuer <40208910@roctilburg.nl>
2025-02-28 10:03:50 +0100

    begonnen aan de create

--- index.php
+++ index.php
@@ -6,7 +6,8 @@ if ($method === "POST") {
     try {
         if (!isset($_POST["token"]) || $_POST["token"] !== "b3f44c1eb885409c222fdb78c125f5e7050ce4f3d15e8b15ffe51678dd3a33d3a18dd3") {
             $_SESSION["error"] = "De token is incorrect";
-        } else {
+        } 
+        else {
             $host = "localhost";
             $username = "root";
             // voor Thom, verander de $password naar root, je gebruikt MAMP
